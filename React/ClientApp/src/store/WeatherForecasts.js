ca236034dff3bdf5adffc2bd9631f90dd6e640ca
theannanas <annakrogh13@hotmail.com>
2021-01-14 11:49:05 +0100

    first commit - react app template

---
+++ React/ClientApp/src/store/WeatherForecasts.js
@@ -0,0 +1,45 @@
+﻿const requestWeatherForecastsType = 'REQUEST_WEATHER_FORECASTS';
+const receiveWeatherForecastsType = 'RECEIVE_WEATHER_FORECASTS';
+const initialState = { forecasts: [], isLoading: false };
+
+export const actionCreators = {
+  requestWeatherForecasts: startDateIndex => async (dispatch, getState) => {    
+    if (startDateIndex === getState().weatherForecasts.startDateIndex) {
+      // Don't issue a duplicate request (we already have or are loading the requested data)
+      return;
+    }
+
+    dispatch({ type: requestWeatherForecastsType, startDateIndex });
+
+    const url = `https://api.statbank.dk/v1/tables`;
+    const response = await fetch(url);
+    const forecasts = await response.json();
+    console.log(forecasts)
+    console.log(response)
+
+    dispatch({ type: receiveWeatherForecastsType, startDateIndex, forecasts });
+  }
+};
+
+export const reducer = (state, action) => {
+  state = state || initialState;
+
+  if (action.type === requestWeatherForecastsType) {
+    return {
+      ...state,
+      startDateIndex: action.startDateIndex,
+      isLoading: true
+    };
+  }
+
+  if (action.type === receiveWeatherForecastsType) {
+    return {
+      ...state,
+      startDateIndex: action.startDateIndex,
+      forecasts: action.forecasts,
+      isLoading: false
+    };
+  }
+
+  return state;
+};
